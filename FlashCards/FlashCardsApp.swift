3512e9d949d0c056df054d7ccac88071ee7ca375
Richie <richard1simonik@gmail.com>
2023-09-13 18:48:16 +0200

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Richard on 13.09.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlashCardsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
