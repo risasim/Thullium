5d3ca036d4e55eccaeedceb4b3da26112b75f38b
Richie <richard1simonik@gmail.com>
2023-09-25 15:35:07 +0200

    Game Engine improved. Now supports selected categories

---
+++ Thullium/Views/Chemistry/ChemistryGroup/ChemistryMenuItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ChemistryMenuItemView.swift
+//  Thullium
+//
+//  Created by Richard on 24.09.2023.
+//
+
+import SwiftUI
+
+struct ChemistryMenuItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChemistryMenuItemView()
+}
