0a90bd99394cff0f3ea1ec26a4f56b14ed42a19b
Richie <richard1simonik@gmail.com>
2023-12-20 14:46:12 +0100

    UI changes

--- Thullium/Views/Settings/SettingsView.swift
+++ Thullium/Views/Settings/SettingsView.swift
@@ -16,7 +16,7 @@ struct SettingsView: View {
                     GroupBox {
                         Divider().padding(.vertical, 4)
                         HStack(alignment: .center, spacing: 10){
-                            Image("AppIcon")
+                            Image(.logo)
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 100, height: 100)
@@ -36,11 +36,13 @@ struct SettingsView: View {
                     }
 // MARK: - Application info
                     GroupBox(content: {
-                        SettingsRowView(label: "Developer", description: "Richie")
-                        SettingsRowView(label: "Compatibility", description: "iOS 17")
-                        SettingsRowView(label: "Twitter", linkLabel: "@richiesimonik", linkDestination: "twitter.com/richiesimonik")
-                        SettingsRowView(label: "SwiftUI", description: "6.0")
                         SettingsRowView(label: "Version",description: "1.2")
+                        SettingsRowView(label: "Compatibility", description: "iOS 17")
+                        SettingsRowView(label: "SwiftUI", description: "6.0")
+                        SettingsRowView(label: "Privacy policy",linkLabel: "Link", linkDestination: "https://www.termsfeed.com/live/5fd31393-40dc-45b7-965d-97601764ffd9")
+                        SettingsRowView(label: "Developer", description: "Richie")
+                        SettingsRowView(label: "Twitter", linkLabel: "@richiesimonik", linkDestination: "twitter.com/richiesimonik")
+                        SettingsRowView(label: "Buy me a cofee", linkLabel: "Link", linkDestination:"https://www.buymeacoffee.com/richiesimonik")
                     }, label: {
                         SettingsLabelView(label: "Application", image: "apps.iphone")
                     })
