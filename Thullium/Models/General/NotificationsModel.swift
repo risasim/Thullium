dae964247df3e7a11dac0ef06e382379971ba33c
Richie <richard1simonik@gmail.com>
2024-01-06 17:43:21 +0100

    ground for notifications

--- Thullium/Models/General/NotificationsModel.swift
+++ Thullium/Models/General/NotificationsModel.swift
@@ -6,7 +6,28 @@
 //
 
 import Foundation
+import UserNotifications
+import SwiftUI
 
-struct NotificationsModel{
-    
+func not_addNotification(title:String, subtitle:String){
+    @AppStorage("notificationsAllowed") var notificationsAllowed:Bool = true
+    if notificationsAllowed{
+        print("notifications got here")
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { _, error in
+            if let error{
+                print("Notification access not granted.", error.localizedDescription)
+                notificationsAllowed = false
+            }
+        }
+        
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = subtitle
+        content.sound = .default
+        
+        print("Notification added")
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request)
+    }
 }
